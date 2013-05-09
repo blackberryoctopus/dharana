81845d39c063822358e3d3bdabf50f05229c740c
John B. Lee <jbl@nextstopgo.com>
2013-05-08 22:18:00 -0400

    Use iCheck for styling checkboxes.

--- static/tasklist.js
+++ static/tasklist.js
@@ -2,8 +2,13 @@ function TaskListCtrl($scope, $http) {
 	$scope.tasks = []
 	$http.get("/dharana/asana/tasks").success(function(dat, stat, xhr) {
 		angular.forEach(dat.data, function(t) {
-			console.log(t.name)
-			$scope.tasks.push({name:t.name, completed:t.completed})
+			$scope.tasks.push({name:t.name, completed:t.completed, id:t.id})
 		})
 	})
+
+	$scope.icheck = function(i) {
+		var taskid = '#task' + $scope.tasks[i].id
+		$(taskid).iCheck({checkboxClass:'icheckbox_minimal-green', radioClass:'iradio_minimal-green'})
+	}
+
 }
