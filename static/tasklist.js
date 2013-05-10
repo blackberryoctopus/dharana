fda809f519d2e80082f18a6460ea2dbe1ef7fbe5
John B. Lee <jbl@nextstopgo.com>
2013-05-10 00:01:55 -0400

    Split list into complete/uncomplete. Move items between lists based on toggle.

--- static/tasklist.js
+++ static/tasklist.js
@@ -1,14 +1,23 @@
 function TaskListCtrl($scope, $http) {
 	$scope.tasks = []
+
 	$http.get("/dharana/asana/tasks").success(function(dat, stat, xhr) {
 		angular.forEach(dat.data, function(t) {
 			$scope.tasks.push({name:t.name, completed:t.completed, id:t.id})
 		})
 	})
 
-	$scope.icheck = function(i) {
-		var taskid = '#task' + $scope.tasks[i].id
-		$(taskid).iCheck({checkboxClass:'icheckbox_minimal-green', radioClass:'iradio_minimal-green'})
+	$scope.setCompleted = function(scope, taskId, completed) {
+		angular.forEach(scope.tasks, function(val,key) {
+			if (val.id == taskId)
+				scope.$apply(scope.tasks[key].completed = completed)
+		})
 	}
 
+	$scope.icheck = function(tid) {
+		var taskid = '#task' + tid
+		$(taskid).iCheck({checkboxClass:'icheckbox_minimal-green', radioClass:'iradio_minimal-green'})
+		$(taskid).on('ifChecked', function(e) { $scope.setCompleted($scope, tid, true) })
+		$(taskid).on('ifUnchecked', function(e) { $scope.setCompleted($scope, tid, false) })
+	}
 }
