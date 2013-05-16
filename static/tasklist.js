aea5449448871754de495d8526d51bbbd750f0f3
John B. Lee <jbl@nextstopgo.com>
2013-05-15 22:21:42 -0400

    Added workspace and project selectors to navbar. Changed to using function that returns task IDs to filter.

--- static/tasklist.js
+++ static/tasklist.js
@@ -1,9 +1,37 @@
+angular.module('Dharana', []).filter('complete', function() {
+	return function(tid) {
+		console.log("Got task " + tid)
+		return true
+	}
+})
+
 function TaskListCtrl($scope, $http) {
-	$scope.tasks = []
+	$scope.currentWorkspace = null
+
+	$scope.taskIds = []
+	$scope.tasks = {}
+	$scope.workspaceIds = []
+	$scope.workspaces = {}
+	$scope.projectsInWorkspace = []
+	$scope.projects = {}
 
 	$http.get("/dharana/asana/tasks").success(function(dat, stat, xhr) {
 		angular.forEach(dat.data, function(t) {
-			$scope.tasks.push({name:t.name, completed:t.completed, id:t.id})
+			$scope.taskIds.push(t.id)
+			$scope.tasks[t.id] = t
+		})
+	})
+
+	$http.get("/dharana/asana/workspaces").success(function(dat, stat, xhr) {
+		angular.forEach(dat.data, function(t) {
+			$scope.workspaceIds.push(t)
+			$scope.workspaces[t.id] = t
+		})
+	})
+
+	$http.get("/dharana/asana/projects").success(function(dat, stat, xhr) {
+		angular.forEach(dat.data, function(t) {
+			$scope.projects[t.id] = t
 		})
 	})
 
@@ -12,6 +40,30 @@ function TaskListCtrl($scope, $http) {
 			if (val.id == taskId)
 				scope.$apply(scope.tasks[key].completed = completed)
 		})
+	}
+
+	$scope.taskIdsByCompletion = function(state) {
+		var completed = []
+		angular.forEach($scope.tasks, function(val) {
+			if (val.completed == state)
+				completed.push(val.id)
+		})
+		return completed
+	}
+
+	$scope.setWorkspace = function(wid) {
+			$scope.currentWorkspace = wid
+			$scope.projectsInWorkspace = []
+			angular.forEach($scope.projects, function(prj) {
+				if (prj.workspace.id == wid) {
+					$scope.projectsInWorkspace.push(prj.id)
+					console.log("project ID " + prj.id + " is in workspace " + wid)
+				}
+			})
+	}
+
+	$scope.dropdown = function() {
+		$('.dropdown-toggle').dropdown()
 	}
 
 	$scope.icheck = function(tid) {
