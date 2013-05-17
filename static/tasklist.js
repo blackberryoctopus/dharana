8e6ae0bc2522cc6e7fdda569dd93662c87e4d298
John B. Lee <jbl@nextstopgo.com>
2013-05-17 01:37:57 -0400

    Add tag popovers. Update workspace/project dropdowns with name of selected workspace/project

--- static/tasklist.js
+++ static/tasklist.js
@@ -7,6 +7,9 @@ angular.module('Dharana', []).filter('complete', function() {
 
 function TaskListCtrl($scope, $http) {
 	$scope.currentWorkspace = null
+	$scope.currentProject = null
+
+	$scope.activeTasks = []
 
 	$scope.taskIds = []
 	$scope.tasks = {}
@@ -15,30 +18,57 @@ function TaskListCtrl($scope, $http) {
 	$scope.projectsInWorkspace = []
 	$scope.projects = {}
 
-	$http.get("/dharana/asana/tasks").success(function(dat, stat, xhr) {
-		angular.forEach(dat.data, function(t) {
-			$scope.taskIds.push(t.id)
-			$scope.tasks[t.id] = t
-		})
-	})
-
 	$http.get("/dharana/asana/workspaces").success(function(dat, stat, xhr) {
 		angular.forEach(dat.data, function(t) {
-			$scope.workspaceIds.push(t)
+			$scope.workspaceIds.push(t.id)
 			$scope.workspaces[t.id] = t
+		})
+
+		$http.get("/dharana/asana/projects").success(function(dat, stat, xhr) {
+			angular.forEach(dat.data, function(t) {
+				$scope.projects[t.id] = t
+			})
+
+			if ($scope.workspaceIds.length > 0) {
+				$scope.setWorkspace($scope.workspaceIds[0])
+			}
 		})
 	})
 
-	$http.get("/dharana/asana/projects").success(function(dat, stat, xhr) {
-		angular.forEach(dat.data, function(t) {
-			$scope.projects[t.id] = t
-		})
-	})
 
 	$scope.setCompleted = function(scope, taskId, completed) {
 		angular.forEach(scope.tasks, function(val,key) {
 			if (val.id == taskId)
 				scope.$apply(scope.tasks[key].completed = completed)
+		})
+	}
+
+	$scope.getTaskTags = function(tid) {
+	}
+
+	$scope.taskDetailPopover = function(tid) {
+		var taskTagId = "taskDetailTags" + tid
+
+		$("#tasklabel" + tid).popover({
+			html: true,
+			placement: "bottom",
+			trigger: "hover",
+			delay: {show:300, hide:200},
+			title: 'Task Detail',
+			content: function() {
+				var taskTagId = "taskTagPopover" + tid
+				$http.get("/dharana/asana/tasks/" + tid + "/tags").success(function(dat, stat, xhr) {
+					var html = ""
+
+					angular.forEach(dat.data, function(tag) {
+						html = html + ' <span class="label">' + tag.name + '</span>'
+					})
+
+					$('#' + taskTagId).html(html)
+				})
+
+				return '<span id="' + taskTagId + '"></span>'
+			}
 		})
 	}
 
@@ -54,11 +84,35 @@ function TaskListCtrl($scope, $http) {
 	$scope.setWorkspace = function(wid) {
 			$scope.currentWorkspace = wid
 			$scope.projectsInWorkspace = []
+
+			$('#workspace-name').text($scope.workspaces[wid].name)
+
 			angular.forEach($scope.projects, function(prj) {
 				if (prj.workspace.id == wid) {
 					$scope.projectsInWorkspace.push(prj.id)
-					console.log("project ID " + prj.id + " is in workspace " + wid)
 				}
+			})
+
+			if ($scope.projectsInWorkspace.length > 0) {
+				$scope.setProject($scope.projectsInWorkspace[0])
+			}
+	}
+
+	$scope.setProject = function(pid) {
+			$scope.currentProject = pid
+			$scope.tasks = {}
+			$scope.taskIds = []
+			$scope.activeTasks = []
+
+			$('#project-name').text($scope.projects[pid].name)
+
+			$http.get("/dharana/asana/projects/" + pid + "/tasks").success(function(dat, stat, xhr) {
+				angular.forEach(dat.data, function(t) {
+					$scope.taskIds.push(t.id)
+					$scope.tasks[t.id] = t
+				})
+
+				$scope.activeTasks = $scope.taskIdsByCompletion(false)
 			})
 	}
 
