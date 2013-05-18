5e867b814b0520745e13edb93bb011539ab5110e
John B. Lee <j.benlin.lee@gmail.com>
2013-05-18 19:44:30 -0400

    Add popup with start/pause/complete controls

--- chrome/background.js
+++ chrome/background.js
@@ -1,18 +1,59 @@
-function checkAsanaTask(tab) {
-	console.log(tab.url)
-	if (tab.url.substr(0,24) === "https://app.asana.com/0/")
-		var asanaTaskUrl = tab.url.substr(24).split('/')
-		var numInts = 0
-		for (var i = 0; i < asanaTaskUrl.length; ++i) {
-			asanaTaskUrl[i] = parseInt(asanaTaskUrl[i])
-			if (!isNaN(asanaTaskUrl[i])) {
-				++numInts
-			}
-		}
+var asanaTaskPattern = /^https:\/\/app\.asana\.com\/0\/\d+\/\d+$/
+var currentTaskId = null
 
-		if (asanaTaskUrl.length == numInts && numInts == 2) {
+function getTask(taskid, callback) {
+	console.log("Getting task data")
+
+	// Get main task data
+	$.getJSON('https://app.asana.com/api/1.0/tasks/' + taskid, function(data) {
+		var currentTask = {type:"task"}
+		console.log("Got main task data: " + JSON.stringify(data.data))
+		currentTask.task = data.data
+
+		// Get task stories
+		$.getJSON('https://app.asana.com/api/1.0/tasks/' + taskid + '/stories', function(data) {
+			console.log("Got task stories: " + JSON.stringify(data.data))
+			currentTask.stories = data.data
+
+			console.log("Calling callback: " + JSON.stringify(currentTask))
+			callback(currentTask)
+		})
+	})
+}
+
+function addStory(taskid, storyText, callback) {
+	$.post('https://app.asana.com/api/1.0/tasks/' + taskid + '/stories',
+		{"text":storyText},
+		function(data, status, xhr) {
+			callback(data)
+		},
+		"json")
+}
+
+function checkAsanaTask(tab) {
+	if (asanaTaskPattern.test(tab.url)) {
+		var taskComponents = tab.url.substr(24).split('/')
+		if (taskComponents[0] != taskComponents[1]) {
 			chrome.pageAction.show(tab.id)
+			currentTaskId = taskComponents[1]
 		}
+	}
 }
 
 chrome.tabs.onActivated.addListener(function(info) { chrome.tabs.get(info.tabId, checkAsanaTask) })
+chrome.tabs.onUpdated.addListener(function(id, info, tab) { checkAsanaTask(tab) })
+
+chrome.runtime.onMessage.addListener(function(msg, sender, resp) {
+	console.log("Got a message: " + JSON.stringify(msg))
+	switch(msg.msg) {
+		case "gettask":
+			getTask(currentTaskId, function(dat) { resp(dat) })
+			return true
+		case "starttask":
+			addStory(msg.data.taskid, "dharana_start", resp)
+			return true
+		case "pausetask":
+			addStory(msg.data.taskid, "dharana_end", resp)
+			return true
+	}
+})
---
+++ chrome/popup.js
@@ -0,0 +1,70 @@
+startTask = function() {
+	chrome.runtime.sendMessage({msg:"starttask",data:{taskid:currentTask.task.id}}, function(resp) {
+		setTaskStateUI("ACTIVE")
+	})
+}
+
+pauseTask = function() {
+	chrome.runtime.sendMessage({msg:"pausetask",data:{taskid:currentTask.task.id}}, function(resp) {
+		setTaskStateUI("HOLD")
+	})
+}
+
+getTaskState = function(task) {
+	var state = "UNKNOWN" // -> NOT_STARTED -> ACTIVE -> HOLD -> COMPLETED
+	var starts = 0
+	var pauses = 0
+
+	if (task.task.completed == false) {
+		$.each(task.stories, function(idx, story) {
+			if (/^dharana_start/.test(story.text)) {
+				++starts
+			} else if (/^dharana_end/.test(story.text)) {
+				++pauses
+			}
+
+			if (starts <= 0) {
+				state = "NOT_STARTED"
+			} else {
+				state = (starts > pauses ? "ACTIVE" : "INACTIVE")
+			}
+		})
+	} else {
+		state = "COMPLETED"
+	}
+
+	return state
+}
+
+setTaskStateUI = function(state) {
+	if (state == "NOT_STARTED" || state == "HOLD") {
+		$("#starttask").css('display', 'block')
+		$("#pausetask").css('display', 'none')
+		$("#taskcomplete").css('display', 'none')
+	} else if (state == "ACTIVE") {
+		$("#starttask").css('display', 'none')
+		$("#pausetask").css('display', 'block')
+		$("#taskcomplete").css('display', 'none')
+	} else if (state == "COMPLETED") {
+		$("#starttask").css('display', 'none')
+		$("#pausetask").css('display', 'none')
+		$("#taskcomplete").css('display', 'block')
+	}
+}
+
+$(window).load(function() {
+	// Setup handlers
+	$("#starttask").click(function(evt) {
+		startTask()
+	})
+
+	$("#pausetask").click(function(evt) {
+		pauseTask()
+	})
+
+	console.log("Requesting task data")
+	chrome.runtime.sendMessage({msg:"gettask"}, function(task) {	
+		currentTask = task
+		setTaskStateUI(getTaskState(task))
+	})
+})
