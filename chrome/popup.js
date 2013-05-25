98e459c7b08e05bda204bea115662087169f4a1c
John B. Lee <jbl@nextstopgo.com>
2013-05-25 11:58:17 -0400

    Added ability to recall currently active tasks

--- chrome/popup.js
+++ chrome/popup.js
@@ -6,11 +6,36 @@ $(window).load(function() {
 	chrome.runtime.sendMessage({msg:Dharana.MSG_QT_TASKS}, function(taskList) {
 		Dharana.dlog('Got task list: ' + JSON.stringify(taskList))
 		$.each(taskList.activeTasks, function(idx, task) {
-			$("#tasks").append('<li class="task-active">' + task.name + '</li>')
+			var taskListItem = $('<li>', {
+				class: 'task-active',
+			})
+
+			var taskNameSpan = $('<span>', {text: task.name}).appendTo(taskListItem)
+			var taskLinkSpan = $('<span>', {
+				class: 'link'
+			})
+
+			var taskLinkIcon = $('<i>', {class: 'icon-chevron-right icon-white'}).appendTo(taskLinkSpan)
+			taskLinkIcon.on('click', function(e) {
+				chrome.tabs.query({active:true, currentWindow:true}, function(tabs) {
+					if (tabs.length > 0) {
+						chrome.tabs.update(tabs[0].id, {url: task.link})
+					} else {
+						Dharana.dlog("Couldn't get active tab")
+					}
+				})
+			})
+
+			taskLinkSpan.appendTo(taskListItem)
+
+			taskListItem.appendTo('#tasks')
 		})
 
 		$.each(taskList.startedTasks, function(idx, task) {
-			$("#tasks").append('<li class="task-started">' + task.name + '</li>')
+			$('<li>', {
+				class: 'task-started',
+				text: task.name
+			}).appendTo('#tasks')
 		})
 	})
 })
