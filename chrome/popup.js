09fbe074751ee6f97d64e6e5e92fe8e305fe3d3b
jbenlinlee <j.benlin.lee@gmail.com>
2013-05-25 19:26:26 -0400

    Close popup when navigating to a task

--- chrome/popup.js
+++ chrome/popup.js
@@ -36,6 +36,7 @@ function createTaskListItem(task, taskState) {
 		chrome.tabs.query({active:true, currentWindow:true}, function(tabs) {
 			if (tabs.length > 0) {
 				chrome.tabs.update(tabs[0].id, {url: task.link})
+				window.close()
 			} else {
 				Dharana.dlog("Couldn't get active tab")
 			}
