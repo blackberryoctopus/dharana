b4e0a4f533658509c1c5523e0d88abc22bf85a7d
John B. Lee <jbl@nextstopgo.com>
2013-05-25 12:06:35 -0400

    Extend links to started as well as active tasks. Pull task list item creation into separate function.

--- chrome/popup.js
+++ chrome/popup.js
@@ -1,41 +1,52 @@
 var currentTask = {}
 
+function createTaskListItem(task, taskState) {
+	var taskClass = ''
+	switch(taskState) {
+		case "active":
+			taskClass = 'task-active'
+			break
+		case "started":
+			taskClass = 'task-started'
+			break
+	}
+
+	var taskListItem = $('<li>', {
+		class: taskClass
+	})
+
+	var taskNameSpan = $('<span>', {text: task.name}).appendTo(taskListItem)
+	var taskLinkSpan = $('<span>', {
+		class: 'link'
+	})
+
+	var taskLinkIcon = $('<i>', {class: 'icon-chevron-right icon-white'}).appendTo(taskLinkSpan)
+	taskLinkIcon.on('click', function(e) {
+		chrome.tabs.query({active:true, currentWindow:true}, function(tabs) {
+			if (tabs.length > 0) {
+				chrome.tabs.update(tabs[0].id, {url: task.link})
+			} else {
+				Dharana.dlog("Couldn't get active tab")
+			}
+		})
+	})
+
+	taskLinkSpan.appendTo(taskListItem)
+
+	return taskListItem
+}
+
 $(window).load(function() {
 	Dharana.LOGNAME = 'dharana-popup'
 	Dharana.dlog("Requesting task data")
 	chrome.runtime.sendMessage({msg:Dharana.MSG_QT_TASKS}, function(taskList) {
 		Dharana.dlog('Got task list: ' + JSON.stringify(taskList))
 		$.each(taskList.activeTasks, function(idx, task) {
-			var taskListItem = $('<li>', {
-				class: 'task-active',
-			})
-
-			var taskNameSpan = $('<span>', {text: task.name}).appendTo(taskListItem)
-			var taskLinkSpan = $('<span>', {
-				class: 'link'
-			})
-
-			var taskLinkIcon = $('<i>', {class: 'icon-chevron-right icon-white'}).appendTo(taskLinkSpan)
-			taskLinkIcon.on('click', function(e) {
-				chrome.tabs.query({active:true, currentWindow:true}, function(tabs) {
-					if (tabs.length > 0) {
-						chrome.tabs.update(tabs[0].id, {url: task.link})
-					} else {
-						Dharana.dlog("Couldn't get active tab")
-					}
-				})
-			})
-
-			taskLinkSpan.appendTo(taskListItem)
-
-			taskListItem.appendTo('#tasks')
+			createTaskListItem(task, "active").appendTo('#tasks')
 		})
 
 		$.each(taskList.startedTasks, function(idx, task) {
-			$('<li>', {
-				class: 'task-started',
-				text: task.name
-			}).appendTo('#tasks')
+			createTaskListItem(task, "started").appendTo('#tasks')
 		})
 	})
 })
