a265a0d7e30d58a0617a52c76b70eeb53d2dc891
jbenlinlee <j.benlin.lee@gmail.com>
2013-06-21 19:14:44 -0400

    Requeue requests if they fail due to rate limiting

--- chrome/asana_gateway.js
+++ chrome/asana_gateway.js
@@ -1,13 +1,15 @@
 function AsanaGateway() {
-	this.rateLimit = 80 / 60000  // Default rate (req/ms) = 80 req / min
-	this.tokenLimit = 80
-	this.tokenCount = 80
+	this.rateLimit = 90 / 60000  // Default rate (req/ms) = 80 req / min
+	this.tokenLimit = 90
+	this.tokenCount = 90
 	this.lastRequestTime = Date.now()
 	this.endpoint = 'https://app.asana.com/api/1.0'
 	this.queuedTasks = 0
 }
 
 AsanaGateway.prototype.request = function(req, callback, data) {
+	var self = this
+
 	if (this.tokenCount < this.tokenLimit) {
 		var earnedTokens = (Date.now() - this.lastRequestTime) * this.rateLimit
 		var updatedTokens = this.tokenCount + earnedTokens
@@ -27,13 +29,21 @@ AsanaGateway.prototype.request = function(req, callback, data) {
 			// GET request
 			$.getJSON(this.endpoint + req, function(data) {
 				callback(data)
+			}).fail(function(xhr, textStatus, errMsg) {
+				Dharana.dlog('Asana request failed with status code ' + xhr.status)
+				switch (xhr.status) {
+					case 429:
+						var retryTimeout = xhr.getResponseHeader('Retry-After')
+						Dharana.dlog('Retrying request ' + req + ' in ' + retryTimeout + 's')
+						setTimeout(function() { self.request(req, callback, data) }, retryTimeout * 1000)
+						break
+				}
 			})
 		}	
 	} else {
 		var waitTime = (1 / this.rateLimit) * (this.queuedTasks + 1)
 		Dharana.dlog('Rate limit reached. Bucket has ' + this.tokenCount + ' tokens. Waiting ' + waitTime + ' ms with ' + this.queuedTasks + ' tasks in the queue.')
 
-		var self = this
 		setTimeout(function() {
 			self.queuedTasks -= 1
 			self.request(req, callback, data)
